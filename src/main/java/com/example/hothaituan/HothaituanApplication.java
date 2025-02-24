b4f12de81703d13f0761fb1d98f279dc096b7273
hothaituan <hothaituan5@gmail.com>
2025-02-24 10:29:48 +0700

    LTWeb2

---
+++ src/main/java/com/example/hothaituan/HothaituanApplication.java
@@ -0,0 +1,13 @@
+package com.example.hothaituan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HothaituanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HothaituanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hothaituan/HothaituanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hothaituan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HothaituanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
